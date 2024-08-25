3d2811d50c6cff28156a9c4c9ddf339b8e532da1
Evan <158116844+Gnauk841026@users.noreply.github.com>
2024-08-25 23:02:31 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-cd /path/to/deployment
+set -e
+set -x
+
+# 確保我們在正確的目錄中
+cd ~/cloudbuild_test_control_VM
+
+# 安裝依賴
 npm install
-pm2 stop all
+
+# 停止所有正在運行的 PM2 進程
+pm2 stop all || true
+
+# 啟動應用
 pm2 start app.js --name "cloud-run-demo"
+
+# 保存 PM2 進程列表，以便在系統重啟時自動啟動
+pm2 save
+
+# 設置 PM2 在系統啟動時自動運行
+pm2 startup
